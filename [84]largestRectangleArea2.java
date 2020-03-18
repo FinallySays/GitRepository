6715edd18490b480af72ebdad1def060bba14a58
FinallySays <shiwplee@gmail.com>
2020-03-18 21:33:59 +0800

    顺序栈

---
+++ [84]largestRectangleArea2.java
@@ -0,0 +1,47 @@
+//给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。 
+//
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。 
+//
+// 
+//
+// 
+//
+// 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。 
+//
+// 
+//
+// 
+//
+// 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: [2,1,5,6,2,3]
+//输出: 10 
+// Related Topics 栈 数组
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int size = heights.length;
+        int area = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < size; ++i) {
+            while (stack.peek() != -1 && heights[i] < heights[stack.peek()]) {
+                area = Math.max(area, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            area = Math.max(area, heights[stack.pop()] * (size - 1 - stack.peek()));
+        }
+        return area;
+    }
+    
+    
+}
+//leetcode submit region end(Prohibit modification and deletion)
