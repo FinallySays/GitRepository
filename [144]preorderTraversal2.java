7bd8e7ca473d859779d1186b21ab4edeaa82f8de
FinallySays <shiwplee@gmail.com>
2020-03-22 19:37:42 +0800

    iterative O(N) 0ms

---
+++ [144]preorderTraversal2.java
@@ -0,0 +1,45 @@
+//给定一个二叉树，返回它的 前序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [1,2,3]
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.empty()) {
+            while (curr != null) {
+                stack.push(curr);
+                list.add(curr.val);
+                curr = curr.left;
+            }
+            curr = stack.pop().right;
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
