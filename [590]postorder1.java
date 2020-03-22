e5242270b4815901696308e9ccbeb530bce06543
FinallySays <shiwplee@gmail.com>
2020-03-22 20:23:04 +0800

    recursive O(N) 3ms

---
+++ [590]postorder1.java
@@ -0,0 +1,49 @@
+//给定一个 N 叉树，返回其节点值的后序遍历。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其后序遍历: [5,6,3,2,4,1]. 
+//
+// 
+//
+// 说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        Node curr = root;
+        if (curr != null) {
+            for (Node node : curr.children)
+                if (node != null) list.addAll(postorder(node));
+            list.add(curr.val);
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
