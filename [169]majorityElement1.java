d57f0511ac59c965cc19e8e86e8a3224038bcbec
FinallySays <shiwplee@gmail.com>
2020-03-26 17:58:26 +0800

    hashmap O(N) 37ms

---
+++ [169]majorityElement1.java
@@ -0,0 +1,35 @@
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [3,2,3]
+//输出: 3 
+//
+// 示例 2: 
+//
+// 输入: [2,2,1,1,1,2,2]
+//输出: 2
+// 
+// Related Topics 位运算 数组 分治算法
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i))
+                map.put(i, map.get(i) + 1);
+            else
+                map.put(i, 1);
+            if (map.get(i) > nums.length / 2)
+                return i;
+        }
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
