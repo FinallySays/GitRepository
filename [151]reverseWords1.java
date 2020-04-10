c7bc677cce20a8d3e270a14cc2de460fd4b65472
FinallySays <shiwplee@gmail.com>
2020-04-10 20:08:01 +0800

    暴力 O（N）5ms

---
+++ [151]reverseWords1.java
@@ -0,0 +1,63 @@
+//给定一个字符串，逐个翻转字符串中的每个单词。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入: "the sky is blue"
+//输出: "blue is sky the"
+// 
+//
+// 示例 2： 
+//
+// 输入: "  hello world!  "
+//输出: "world! hello"
+//解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+// 
+//
+// 示例 3： 
+//
+// 输入: "a good   example"
+//输出: "example good a"
+//解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+// 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 无空格字符构成一个单词。 
+// 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。 
+// 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 请选用 C 语言的用户尝试使用 O(1) 额外空间复杂度的原地解法。 
+// Related Topics 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < chars.length; ++i) {
+            if (i > 0 && chars[i] == ' ' && chars[i - 1] == ' ')
+                continue;
+            else
+                b.append(chars[i]);
+        }
+        s = b.toString();
+        String[] strs = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strs.length - 1; i >= 0; --i) {
+                sb.append(strs[i].trim()).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
