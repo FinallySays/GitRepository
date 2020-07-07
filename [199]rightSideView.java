9ae5a965410dec0414e9f809ec870da02bc7aa25
FinallySays <shiwplee@gmail.com>
2020-07-07 18:57:42 +0800

    O(N) 0ms

---
+++ [199]rightSideView.java
@@ -0,0 +1,43 @@
+//给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 示例: 
+//
+// 输入: [1,2,3,null,5,null,4]
+//输出: [1, 3, 4]
+//解释:
+//
+//   1            <---
+// /   \
+//2     3         <---
+// \     \
+//  5     4       <---
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, 0, result);
+        return result;
+    }
+
+    private void dfs(TreeNode curr, int level,List<Integer> result) {
+        if (curr == null) return;
+        if (level == result.size())
+            result.add(curr.val);
+        dfs(curr.right, level + 1, result);
+        dfs(curr.left, level + 1, result);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
